0b80cb1dfc0e89e412f7788b0a97d3f073af91c5
Bradley J. Spaulding <brad.spaulding@gmail.com>
2018-12-06 20:59:19 -0800

    Fixed early return outside of function
    not sure why this failed to compile... 🤔

--- src/index.js
+++ src/index.js
@@ -21,75 +21,80 @@ const removeLoaderAndShowBells = () => {
 	showBells();
 };
 const contextConstructor = window.AudioContext || window.webkitAudioContext;
+
+function main() {
+	const audioContext = new contextConstructor();
+	const importAudio = url =>
+		new Promise((resolve, reject) => {
+			let req = new XMLHttpRequest();
+			req.open('GET', encodeURI(url), true);
+			req.responseType = 'arraybuffer';
+			req.onload = () => {
+				audioContext.decodeAudioData(req.response, resolve, reject);
+			};
+			req.onerror = reject;
+			req.send();
+		});
+	let loadingP = [];
+
+	function buildReverbImpulse(context) {
+		const rate = context.sampleRate;
+		const length = rate * 2;
+		const decay = 3.4;
+		const impulse = context.createBuffer(2, length, rate)
+		const impulseL = impulse.getChannelData(0)
+		const impulseR = impulse.getChannelData(1)
+
+		for (let i = 0; i < length; i += 1) {
+			impulseL[i] = (Math.random() * 2 - 1) * Math.pow(1 - i / length, decay);
+			impulseR[i] = (Math.random() * 2 - 1) * Math.pow(1 - i / length, decay);
+		}
+
+		return impulse;
+	}
+	const reverb = audioContext.createConvolver();
+	reverb.buffer = buildReverbImpulse(audioContext);
+	const reverbGain = audioContext.createGain();
+	reverb.connect(reverbGain);
+	reverbGain.connect(audioContext.destination);
+	reverbGain.gain.value = 0.7;
+
+	const touchSupported = 'ontouchstart' in window;
+	document.querySelectorAll('.bell').forEach(button => {
+		const note = button.getAttribute('data-note');
+		let sampleData = null;
+		loadingP.push(
+			importAudio(notes[note]).then(buffer => {
+				sampleData = buffer;
+				button.addEventListener(
+					touchSupported ? 'touchstart' : 'mousedown',
+					() => {
+						const source = audioContext.createBufferSource();
+						source.buffer = sampleData;
+						source.connect(audioContext.destination);
+						source.connect(reverb);
+
+						button.classList.add('ring');
+						source.start(0);
+						setTimeout(() => {
+							button.classList.remove('ring');
+						}, 1000);
+					},
+					{ passive: true }
+				);
+			})
+		);
+	});
+	Promise.all(loadingP)
+		.then(removeLoaderAndShowBells)
+		.catch(error => {
+			removeLoader();
+		});
+}
+
 if (!contextConstructor) {
 	removeLoader();
 	document.querySelector('#not-supported').style.display = 'block';
-	return;
+} else {
+	main();
 }
-const audioContext = new contextConstructor();
-const importAudio = url =>
-	new Promise((resolve, reject) => {
-		let req = new XMLHttpRequest();
-		req.open('GET', encodeURI(url), true);
-		req.responseType = 'arraybuffer';
-		req.onload = () => {
-			audioContext.decodeAudioData(req.response, resolve, reject);
-		};
-		req.onerror = reject;
-		req.send();
-	});
-let loadingP = [];
-
-function buildReverbImpulse(context) {
-	const rate = context.sampleRate;
-	const length = rate * 2;
-	const decay = 3.4;
-	const impulse = context.createBuffer(2, length, rate)
-	const impulseL = impulse.getChannelData(0)
-	const impulseR = impulse.getChannelData(1)
-
-	for (let i = 0; i < length; i += 1) {
-		impulseL[i] = (Math.random() * 2 - 1) * Math.pow(1 - i / length, decay);
-		impulseR[i] = (Math.random() * 2 - 1) * Math.pow(1 - i / length, decay);
-	}
-
-	return impulse;
-}
-const reverb = audioContext.createConvolver();
-reverb.buffer = buildReverbImpulse(audioContext);
-const reverbGain = audioContext.createGain();
-reverb.connect(reverbGain);
-reverbGain.connect(audioContext.destination);
-reverbGain.gain.value = 0.7;
-
-const touchSupported = 'ontouchstart' in window;
-document.querySelectorAll('.bell').forEach(button => {
-	const note = button.getAttribute('data-note');
-	let sampleData = null;
-	loadingP.push(
-		importAudio(notes[note]).then(buffer => {
-			sampleData = buffer;
-			button.addEventListener(
-				touchSupported ? 'touchstart' : 'mousedown',
-				() => {
-					const source = audioContext.createBufferSource();
-					source.buffer = sampleData;
-					source.connect(audioContext.destination);
-					source.connect(reverb);
-
-					button.classList.add('ring');
-					source.start(0);
-					setTimeout(() => {
-						button.classList.remove('ring');
-					}, 1000);
-				},
-				{ passive: true }
-			);
-		})
-	);
-});
-Promise.all(loadingP)
-	.then(removeLoaderAndShowBells)
-	.catch(error => {
-		removeLoader();
-	});
